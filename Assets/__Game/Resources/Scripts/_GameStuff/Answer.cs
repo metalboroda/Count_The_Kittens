121f340c7d02a795cbaa9af981daee8c88af696f
DESKTOP-DIRJUMI\VladBoroda <VladBoroda>
2024-07-11 19:12:08 +0300

    New Mechanic

--- Assets/__Game/Resources/Scripts/_GameStuff/Answer.cs
+++ Assets/__Game/Resources/Scripts/_GameStuff/Answer.cs
@@ -1,131 +1,120 @@
-using __Game.Resources.Scripts.EventBus;
-using Assets.__Game.Resources.Scripts.Game.States;
-using Assets.__Game.Scripts.Infrastructure;
 using DG.Tweening;
 using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 namespace Assets.__Game.Resources.Scripts._GameStuff
 {
   public class Answer : MonoBehaviour
   {
-    public event Action NumberButtonClicked;
-    public event Action<int> NumberButtonClickedInt;
+    public event Action<Answer> CatButtonPressed;
+    public event Action<string> AnswerButtonPressed;
 
-    [SerializeField] private Image _catImage;
-    [SerializeField] private Color _defaultColor;
-    [SerializeField] private Color _selectionColor;
-    [Space]
-    [SerializeField] private Color _numberButtonDefaultColor;
-    [SerializeField] private Color _numberButtonSelectionColor;
+    [Header("Settings")]
+    [SerializeField] private string _answerButtonValue;
+    [Header("")]
+    [Header("References")]
+    [Header("Buttons")]
+    [SerializeField] private Button _catButton;
+    [SerializeField] private Button _answerButton;
+    [Header("Texts")]
+    [SerializeField] private TextMeshProUGUI _numberText;
+    [SerializeField] private TextMeshProUGUI _answerButtonText;
     [Header("Tutorial")]
-    [SerializeField] private bool _allowTutorial;
-    [SerializeField] private GameObject _tutorialFingerCat;
-    [SerializeField] private GameObject _tutorialFingerNumber;
-    [field: Header("References")]
-    [field: SerializeField] public Button AnswerButton;
-    [SerializeField] private Button _numberButton;
+    [SerializeField] private bool _tutorial;
+    [SerializeField] private GameObject _catFinger;
+    [SerializeField] private GameObject _answerFinger;
+    [Header("Audio")]
+    [SerializeField] private float _minRandomPitch = 0.95f;
+    [SerializeField] private float _maxRandomPitch = 1.05f;
+    [Header("")]
+    [SerializeField] private AudioClip _meowClip;
 
-    public bool Completed { get; private set; }
-
-    private int _answerNumber;
-
-    private GameBootstrapper _gameBootstrapper;
-    private AnswersContainer _answersContainer;
+    private AudioSource _audioSource;
 
     private void Awake() {
-      _gameBootstrapper = GameBootstrapper.Instance;
-      _answersContainer = GetComponentInParent<AnswersContainer>();
-
-      GetAllCatsSpriteRenderers();
-    }
-
-    private void OnEnable() {
-      AnswerButton.onClick.AddListener(() => {
-        EnableNumberObject();
-
-        AnswerButton.interactable = false;
-
-        ChangeCatsColors();
-
-        EventBus<EventStructs.UiButtonEvent>.Raise(new EventStructs.UiButtonEvent());
-      });
-
-      _numberButton.onClick.AddListener(() => {
-        Completed = true;
-
-        _numberButton.interactable = false;
-
-        if (_allowTutorial == true)
-          _tutorialFingerNumber.SetActive(false);
-
-        ChangeNumberButtonColors();
-
-        NumberButtonClicked?.Invoke();
-        NumberButtonClickedInt?.Invoke(_answerNumber);
-
-        EventBus<EventStructs.UiButtonEvent>.Raise(new EventStructs.UiButtonEvent());
-
-        if (_answerNumber == _answersContainer.CurrentNumber - 1)
-          _gameBootstrapper.StateMachine.ChangeStateWithDelay(new GameWinState(_gameBootstrapper), 1f, this);
-        else
-          _gameBootstrapper.StateMachine.ChangeStateWithDelay(new GameLoseState(_gameBootstrapper), 1f, this);
-      });
+      _audioSource = GetComponent<AudioSource>();
     }
 
     private void Start() {
-      _numberButton.transform.localScale = Vector3.zero;
-      _numberButton.gameObject.SetActive(false);
+      SetAnswerButtonText();
+      SubscribeButtons();
+      HandleTexts();
+      HandleButtons();
+      TutorialSwitch(1);
+    }
 
-      if (_allowTutorial == true) {
-        _tutorialFingerCat.SetActive(true);
+    public void SetNumberText(string text) {
+      _numberText.text = text;
+
+      _numberText.gameObject.SetActive(true);
+      _numberText.GetComponent<DOTweenAnimation>().DOPlay();
+    }
+
+    private void SetAnswerButtonText() {
+      _answerButtonText.text = _answerButtonValue;
+    }
+
+    private void SubscribeButtons() {
+      _catButton.onClick.AddListener(() => {
+        CatButtonPressed?.Invoke(this);
+
+        _catButton.interactable = false;
+
+        PlayAudioClipWithRandomPitch(_meowClip);
+
+        _answerButton.gameObject.SetActive(true);
+        _answerButton.GetComponent<DOTweenAnimation>().DOPlay();
+
+        TutorialSwitch(2);
+      });
+
+      _answerButton.onClick.AddListener(() => {
+        AnswerButtonPressed?.Invoke(_answerButtonText.text);
+
+        TutorialSwitch(3);
+      });
+    }
+
+    private void HandleTexts() {
+      _numberText.gameObject.SetActive(false);
+    }
+
+    private void HandleButtons() {
+      _answerButton.gameObject.SetActive(false);
+    }
+
+    private void PlayAudioClipWithRandomPitch(AudioClip audioClip) {
+      if (_audioSource == null) return;
+
+      float randomPitch = Random.Range(_minRandomPitch, _maxRandomPitch);
+
+      _audioSource.pitch = randomPitch;
+
+      _audioSource.PlayOneShot(audioClip);
+    }
+
+    private void TutorialSwitch(int stage) {
+      _catFinger.SetActive(false);
+      _answerFinger.SetActive(false);
+
+      if (_tutorial == false) return;
+
+      switch (stage) {
+        case 1:
+          _catFinger.SetActive(true);
+          break;
+        case 2:
+          _catFinger.SetActive(false);
+          _answerFinger.SetActive(true);
+          break;
+        case 3:
+          _catFinger.SetActive(false);
+          _answerFinger.SetActive(false);
+          break;
       }
-      else {
-        _tutorialFingerCat.SetActive(false);
-      }
-
-      _tutorialFingerNumber.SetActive(false);
-    }
-
-    private void EnableNumberObject() {
-      _numberButton.gameObject.SetActive(true);
-
-      Vector3 scale = new Vector3(1, 1, 1);
-
-      _numberButton.transform.DOScale(scale, 0.5f);
-
-      if (_allowTutorial == true) {
-        _tutorialFingerCat.SetActive(false);
-        _tutorialFingerNumber.SetActive(true);
-      }
-    }
-
-    public void SetAnswerNumber(int number) {
-      _answerNumber = number;
-
-      _numberButton.GetComponentInChildren<TextMeshProUGUI>().text = _answerNumber.ToString();
-    }
-
-    private void GetAllCatsSpriteRenderers() {
-      _catImage.color = _defaultColor;
-    }
-
-    private void ChangeCatsColors() {
-      Sequence sequence = DOTween.Sequence();
-
-      sequence.Append(_catImage.DOColor(_selectionColor, 0.2f))
-              .Append(_catImage.DOColor(_defaultColor, 0.2f));
-      sequence.Play();
-    }
-
-    private void ChangeNumberButtonColors() {
-      Sequence sequence = DOTween.Sequence();
-
-      sequence.Append(_numberButton.GetComponentInChildren<TextMeshProUGUI>().DOColor(_numberButtonSelectionColor, 0.2f))
-              .Append(_numberButton.GetComponentInChildren<TextMeshProUGUI>().DOColor(_numberButtonDefaultColor, 0.2f));
-      sequence.Play();
     }
   }
 }
--- Assets/__Game/Resources/Scripts/_GameStuff/AnswersContainer.cs
+++ Assets/__Game/Resources/Scripts/_GameStuff/AnswersContainer.cs
@@ -1,87 +1,38 @@
-using __Game.Resources.Scripts.EventBus;
-using Assets.__Game.Scripts.Infrastructure;
-using System.Collections;
 using UnityEngine;
 
 namespace Assets.__Game.Resources.Scripts._GameStuff
 {
   public class AnswersContainer : MonoBehaviour
   {
+    [SerializeField] private string _correctValue;
+    [Header("")]
     [SerializeField] private Answer[] _answers;
-    [Header("Audio")]
-    [SerializeField] private AudioClip[] _numbersClips;
-    [Header("Stupor settings")]
-    [SerializeField] private float _stuporTimeoutSeconds = 30f;
 
-    public int CurrentNumber { get; private set; }
-
-    private Coroutine _stuporTimeoutRoutine;
-
-    private GameBootstrapper _gameBootstrapper;
-
-    private void Awake() {
-      _gameBootstrapper = GameBootstrapper.Instance;
-    }
-
-    private void OnEnable() {
-      foreach (var answer in _answers) {
-        answer.AnswerButton.onClick.AddListener(() => AssignNumber(answer));
-        answer.NumberButtonClicked += CheckForAllAnswersCompleted;
-        answer.NumberButtonClickedInt += (number) => PlayNumberClip(number);
-      }
-    }
-
-    private void OnDisable() {
-      foreach (var answer in _answers) {
-        answer.AnswerButton.onClick.RemoveListener(() => AssignNumber(answer));
-        answer.NumberButtonClicked -= CheckForAllAnswersCompleted;
-        answer.NumberButtonClickedInt -= (number) => PlayNumberClip(number);
-      }
-    }
+    private int _currentTextNumber = 1;
 
     private void Start() {
-      CurrentNumber = 1;
-
-      ResetAndStartStuporTimer();
-
-      EventBus<EventStructs.VariantsAssignedEvent>.Raise(new EventStructs.VariantsAssignedEvent());
+      AnswersSubscription();
     }
 
-    private void AssignNumber(Answer answer) {
-      answer.SetAnswerNumber(CurrentNumber);
-
-      CurrentNumber++;
-    }
-
-    private void CheckForAllAnswersCompleted() {
+    private void AnswersSubscription() {
       foreach (var answer in _answers) {
-        if (answer.Completed == false) {
-          return;
-        }
+        answer.CatButtonPressed += SetAnswerNumberText;
+        answer.AnswerButtonPressed += OnAnswerButton;
       }
-
-      ResetAndStartStuporTimer();
-      StopCoroutine(_stuporTimeoutRoutine);
     }
 
-    private void ResetAndStartStuporTimer() {
-      if (_stuporTimeoutRoutine != null)
-        StopCoroutine(_stuporTimeoutRoutine);
+    private void SetAnswerNumberText(Answer answer) {
+      answer.SetNumberText(_currentTextNumber.ToString());
 
-      _stuporTimeoutRoutine = StartCoroutine(DoStuporTimerCoroutine());
+      _currentTextNumber++;
     }
 
-    private IEnumerator DoStuporTimerCoroutine() {
-      yield return new WaitForSeconds(_stuporTimeoutSeconds);
-
-      EventBus<EventStructs.StuporEvent>.Raise(new EventStructs.StuporEvent());
-
-      ResetAndStartStuporTimer();
-    }
-
-    private void PlayNumberClip(int number) {
-      if (number > 0 && number <= _numbersClips.Length) {
-        EventBus<EventStructs.VariantAudioClickedEvent>.Raise(new EventStructs.VariantAudioClickedEvent { AudioClip = _numbersClips[number - 1] });
+    private void OnAnswerButton(string buttonValue) {
+      if (buttonValue == _correctValue) {
+        Debug.Log("Win");
+      }
+      else {
+        Debug.Log("Lose");
       }
     }
   }
